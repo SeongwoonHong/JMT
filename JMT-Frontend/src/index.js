d4bf3e54e8169d9d2934c1b57ff8ac61814cfc38
seongwoon hong <saytoyou91@gmail.com>
2018-10-26 22:13:09 -0400

    Add test routes

--- JMT-Frontend/src/index.js
+++ JMT-Frontend/src/index.js
@@ -8,11 +8,23 @@ import NotFound from 'components/NotFound';
 import Main from 'modules/Main/Main';
 import './styles';
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+let TestRoutes;
+if (isDevelopment) {
+  TestRoutes = require('./test/routes').default;
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <div>
         <Switch>
+          {
+            isDevelopment
+              ? <Route path="/test" component={ TestRoutes } />
+              : null
+          }
           <Route exact path="/" component={Main} />
           <Route component={NotFound} />
         </Switch>
