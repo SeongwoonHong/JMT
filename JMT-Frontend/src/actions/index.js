bd32649ac4d7199c9cd4227cd36c9f3376ace2fe
edwinyoon <ep160101@gmail.com>
2018-11-11 17:43:01 -0500

    remove filter from index

--- JMT-Frontend/src/actions/index.js
+++ JMT-Frontend/src/actions/index.js
@@ -3,6 +3,5 @@ import * as Alert from './alert';
 import * as Restaurant from './restaurant';
 import * as GoogleMap from './googleMap';
 import * as Auth from './auth';
-import * as Filter from './filter';
 
-export { App, Alert, Restaurant, GoogleMap, Auth, Filter };
+export { App, Alert, Restaurant, GoogleMap, Auth };
