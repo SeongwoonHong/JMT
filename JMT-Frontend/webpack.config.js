32285651d26517cd341f02b4c6b953969f779258
lyndonoc <lyndonoc@gmail.com>
2018-12-18 21:06:28 -0500

    fixes plain css not getting applied

--- JMT-Frontend/webpack.config.js
+++ JMT-Frontend/webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
         test: /\.css$/,
         use: [
           { loader: 'style-loader' },
-          { loader: 'css-loader', options: { modules: true } },
+          { loader: 'css-loader', options: { modules: false } },
         ]
       },
       {
