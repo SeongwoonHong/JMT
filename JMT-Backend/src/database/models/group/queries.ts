ac0663aa8c2e65be2f5e03acf1e438a9cd8f8f79
lyndonoc <lyndonoc@gmail.com>
2019-04-11 20:23:35 -0400

    dockerize and ci

--- JMT-Backend/src/database/models/group/queries.ts
+++ JMT-Backend/src/database/models/group/queries.ts
@@ -13,15 +13,13 @@ class Query {
     return db.executeSP('GET_USERS_BY_GROUP', params);
   };
 
-<<<<<<< HEAD
   public static getGroupsByRestaurant = (params): Promise<any> => {
     return db.executeSP('GET_GROUPS_BY_RESTAURANT', params);
-  }
-=======
+  };
+
   public static getGroupsByRestaurantAvailable = (params): Promise<any> => {
     return db.executeSP('GET_GROUPS_BY_RESTAURANT_AVAILABLE', params);
   };
->>>>>>> 989c1b8... dev group list
 }
 
 export default Query;
